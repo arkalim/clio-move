509cf120c2410ef93a72266518667eaa961a6e6d
Abdur Rahman <abdur.rahman@clio.com>
2024-10-28 12:06:21 -0400

    formatted reminder message

--- src/models/slack.py
+++ src/models/slack.py
@@ -5,18 +5,28 @@ class Slack:
     self.slack_interface = SlackInterface()
 
   def send_reminder(self, channel, exercise):
+    steps_text = "\n".join(f"• {step}" for step in exercise["steps"])
     blocks = [
       {
         "type": "section",
         "text": {
           "type": "mrkdwn",
-          "text": exercise["name"],
+          "text": f"*{exercise['name']}*\n{exercise['description']}\n\n*Steps:*\n{steps_text}",
         }
       },
       {
         "type": "image",
         "image_url": exercise["image_url"],
-        "alt_text": exercise["name"]
+        "alt_text": f"{exercise["name"]} Image"
+      },
+      {
+        "type": "context",
+        "elements": [
+          {
+            "type": "mrkdwn",
+            "text": exercise["image_credit"]
+          }
+        ]
       }
     ]
     return self.slack_interface.send_message(channel, blocks)
