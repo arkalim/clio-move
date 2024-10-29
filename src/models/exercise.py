b5d8665aa1decbbe08ce8f070409868e3144469e
Abdur Rahman <abdur.rahman@clio.com>
2024-10-29 16:04:22 -0400

    random exercise

--- src/models/exercise.py
+++ src/models/exercise.py
@@ -1,8 +1,9 @@
 import json
+import random
 
 class Exercise:
   exercises = json.load(open("data/exercises.json"))
 
   @classmethod
   def get_random(self):
-    return self.exercises[0]
+    return random.choice(cls.exercises)
