c915b60fd8d4bc9de389baef53019282ab73ac81
Abdur Rahman <abdur.rahman.kalim@gmail.com>
2024-10-31 03:26:42 +0000

    added exception handling to the daemon thread

--- src/utils/scheduler.py
+++ src/utils/scheduler.py
@@ -13,14 +13,20 @@ START_WORK_HOUR = 9
 END_WORK_HOUR = 17
 
 def schedule_reminders():
-  exercise = Exercise.get_random()
-  print(f"Scheduling reminders for exercise: {exercise["name"]}")
-  for user in User.get_all():
-    current_time = datetime.utcfromtimestamp(int(time.time()) + user.tz_offset)
-    if START_WORK_HOUR <= current_time.hour < END_WORK_HOUR:
-      slack.send_reminder(user.id, exercise)
-      print(f"Sent reminder to {user.name} at local time {current_time}")
-      time.sleep(1)
+  try:
+    exercise = Exercise.get_random()
+    print(f"Scheduling reminders for exercise: {exercise["name"]}")
+    for user in User.get_all():
+      try:
+        current_time = datetime.utcfromtimestamp(int(time.time()) + user.tz_offset)
+        if START_WORK_HOUR <= current_time.hour < END_WORK_HOUR:
+          slack.send_reminder(user.id, exercise)
+          print(f"Sent reminder to {user.name} at local time {current_time}")
+          time.sleep(1)
+      except:
+        print(f"Failed to schedule reminder to {user.name} at local time {current_time}")
+  except Exception as e:
+    print(f"Failed to schedule reminders for exercise: {exercise["name"]}")
 
 def run_scheduler():
   schedule.every(1).minutes.do(schedule_reminders)
